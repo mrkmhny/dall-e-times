caf886a6d8f80721646b36cf6092fc0b05b88568
Mark Mahoney <mark@Marks-MacBook-Pro-Max.local>
2023-11-19 15:07:55 -0800

    get data from api and save it in database

---
+++ datacollector/main.js
@@ -0,0 +1,29 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const fetch = require('node-fetch');
+
+const firestoreServiceAccount = require('./private/dall-e-times.json');
+const newsIoAccount = require('./private/newsdata-io.json');
+
+initializeApp({
+  credential: cert(firestoreServiceAccount),
+});
+
+async function main() {
+    
+    const db = getFirestore();
+    
+    try {
+        const date = new Date()
+
+        const recentNewsRes = await fetch(`https://newsdata.io/api/1/news?apikey=${newsIoAccount.apiKey}&category=science&country=us`)
+        const recentNewsData = await recentNewsRes.json()
+
+        const docRef = db.collection('news').doc(date.getTime().toString());
+        await docRef.set(recentNewsData);
+    } catch (err) {
+        console.error("Something bad happened:", err)
+    }
+}
+
+main();
